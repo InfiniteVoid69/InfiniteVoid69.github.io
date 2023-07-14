8d376693247200b34f0b1c8e5c13f5c0fa847505
InfiniteVoid69 <ADJordan305@hotmail.com>
2023-07-13 19:15:21 -0500

    Hopefully this works

--- login.php
+++ login.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="shortcut icon" href="./images/icon.png" type="image/x-icon">
     <link rel="stylesheet" href="./src/style.css">
-    <link rel="icon" type="image/x-icon" href="./images/favicon.ico">
 </head>
 <body>
 
