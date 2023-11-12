742c16a3f3fbcbd78e838a7b7d52f6e81d1ac1b3
InfiniteVoid69 <62683835+InfiniteVoid69@users.noreply.github.com>
2023-11-11 23:09:00 -0800

    .

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ $(function(){
     $("#nav-placeholder").load("./assets/nav.html");
 });
 
-$('#nav-placeholder').load('./assets/nav.html');
+$("#nav-placeholder").click(function() {
+    alert("worked")
+});
+
+// $('#nav-placeholder').load('./assets/nav.html');
