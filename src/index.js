19114641b8fc9d6faf8be9f4949f0f360cc95bcb
InfiniteVoid69 <62683835+InfiniteVoid69@users.noreply.github.com>
2023-11-11 23:05:52 -0800

    .

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ $(function(){
     $("#nav-placeholder").load("./assets/nav.html");
 });
 
-$('#targetid').load('./assets/nav.html');
-
-window.onload = function () {
-    document.getElementById("nav-placeholder").innerHTML='<object type="text/html" data="./asstes/nav.html" ></object>';
-}
+$('#nav-placeholder').load('./assets/nav.html');
